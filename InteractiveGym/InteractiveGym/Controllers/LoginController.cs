db0d246d88e438d68285e2c5c2522cbb148a93c3
GUNJack1994 <lukasz.barczykowski94@gmail.com>
2019-02-14 21:59:44 +0100

    log

---
+++ InteractiveGym/InteractiveGym/Controllers/LoginController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheGym.Models;
+
+namespace InteractiveGym.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(TheGym.Models.User userModel)
+        {
+            using (GymDb models = new GymDb())
+            {
+                var userDetails = models.tabUser.Where(x => x.Login == userModel.Login && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.Id;
+                    Session["userName"] = userDetails.Login;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+        }
+
+        public ActionResult LogOut()
+        {
+            int userId = (int)Session["userID"];
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+
+
+    }
+}
---
+++ InteractiveGym/InteractiveGym/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TheGym.Models;
+
+namespace InteractiveGym.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        // GET: User
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            User userModel = new User();
+            return View(userModel);
+        }
+        [HttpPost]
+
+        public ActionResult AddOrEdit(User userModel)
+        {
+
+            using (GymDb models = new GymDb())
+            {
+
+                if (models.tabUser.Any(x => x.Login == userModel.Login))
+                {
+
+                    ViewBag.DuplicateMessage = "Użytkownik istnieje";
+                    return View("AddOrEdit", userModel);
+
+                }
+
+                models.tabUser.Add(userModel);
+                models.SaveChanges();
+
+
+            }
+            ModelState.Clear();
+            ViewBag.SuccesMessage = "Resjestracja zakończona powodzeniem";
+            return View("AddOrEdit", new User());
+
+        }
+
+    }
+}
